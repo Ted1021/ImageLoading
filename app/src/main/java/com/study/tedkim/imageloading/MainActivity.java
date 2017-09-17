2f188db69a387d89d0d7b94e0bb9b7630d9b18c9
Ted <kimtx2017@gmail.com>
2017-09-17 14:01:24 +0900

    Add config
    - 화면 회전 / 스크린 크기 변화에 따른 동작 제한설정
    - SDK 버전정보 표기

--- app/src/main/java/com/study/tedkim/imageloading/MainActivity.java
+++ app/src/main/java/com/study/tedkim/imageloading/MainActivity.java
@@ -1,8 +1,12 @@
 package com.study.tedkim.imageloading;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +19,26 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String VIEWER_URL = "http://www.gettyimagesgallery.com/collections/archive/slim-aarons.aspx";
+    private static final String BASE_URL = "http://www.gettyimagesgallery.com";
+
     private ArrayList<String> mImgUrls = new ArrayList<>();
+    private ArrayList<Bitmap> mDataset = new ArrayList<>();
+    private HashMap<String, String> mImageFileSet = new HashMap<>();
 
     Button mDownloadImage, mSetImage;
+    RecyclerView mImageList;
+    ImageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initView();
-
+        setRecyclerView();
     }
 
     private void initView(){
@@ -38,6 +55,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mSetImage = (Button) findViewById(R.id.button_setImages);
         mSetImage.setOnClickListener(this);
+
+        mImageList = (RecyclerView) findViewById(R.id.recyclerView_imageList);
+    }
+
+    private void setRecyclerView(){
+
+        mAdapter = new ImageAdapter(MainActivity.this, mDataset);
+        mImageList.setAdapter(mAdapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mImageList.setLayoutManager(layoutManager);
     }
 
     @Override
@@ -49,7 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.button_setImages:
-
                 break;
         }
     }
@@ -72,11 +99,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Elements imgs = document.select(".picture");
 
                     for(Element img : imgs){
-                        mImgUrls.add(img.attr("src"));
-                        Log.d("CHECK_IMG_URL", img.attr("src"));
+                        String[] components = img.attr("src").split("/");
+                        mImageFileSet.put(components[2], components[3]);
                     }
 
                 } catch (IOException e) {
+                    Log.e("FAIL_GET_IMAGE_RES", "Fail to get image urls");
                     e.printStackTrace();
                 }
 
@@ -90,5 +118,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, "Download complete", Toast.LENGTH_SHORT).show();
             }
         }.execute();
+    }
+
+    private void setData(){
+
+
+
+    }
+
+    private void downloadImages(String no, String fileName){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .build();
+
+        ImageLoadingService service = retrofit.create(ImageLoadingService.class);
+        Call<ResponseBody> call = service.imageList(no, fileName);
+        call.enqueue(new Callback<ResponseBody>() {
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()){
+                    if(response.body() != null){
+                        Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                        mDataset.add(bitmap);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+
+        mAdapter.notifyDataSetChanged();
     }
 }
